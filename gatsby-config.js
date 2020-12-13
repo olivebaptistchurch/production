a6b6fdcd89d453a4592b83a9f941f2e9e9a8a1ce
Colton Plumley <colton@hipcreativeinc.com>
2020-12-13 16:44:30 -0600

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-google-analytics`,
